321c1d995032fbbb5ee3615d4e6e8360dd3cfabe
Ryan <ryan.fee625@gmail.com>
2020-03-26 11:25:20 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+module "instance" {
+	source  = "my.scalr.com/org-sgncvo4mr5l4na0/instance_module/aws"
+	version = "0.0.1"
+  instance_type = var.instance_type
+  instance_count = var.instance_count
+  subnet = var.subnet
+  sg = var.security_group
+  key = var.ssh_key
+  vpc_id = var.vpc_id
+  ami = var.ami
+}
